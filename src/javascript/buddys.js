ed7a6fd5b07a2a36a9102706500774b7ec2dccd4
Jessica Loers <jessica.loers@gmail.com>
2021-11-26 18:33:33 +0100

    added js functions and more

--- src/javascript/buddys.js
+++ src/javascript/buddys.js
@@ -1,13 +1,30 @@
-const buddys = [['Align', 'Block'], [Center, Descendent], [End, Focus], [Grid, Hover], [Inline, Justify]]
+const buddys = [
+  ["Align", "Block"], // 0 --> [0] [1]
+  ["Center", "Descendent"], // 1 --> [0] [1]
+  ["End", "Focus"], // 2 --> [0] [1]
+  ["Grid", "Hover"], // 3 --> [0] [1]
+  ["Inline", "Justify"], // 4 --> [0] [1]
+];
 
-// buddys.forEach((person, index) => {
-//     console.log(person);
-//     let container = document.querySelector('.small-flexbox');
-//     let personBox = document.querySelector('.members')
-//     container.appendChild(personBox)
-//     personBox.classList
-            
-            
-// })
-    
-console.log(buddys[0]); 
+const mainContainer = document.querySelector("main");
+
+buddys.forEach((pair, indexOne) => {
+  const sectionContainer = document.createElement("section");
+  mainContainer.appendChild(sectionContainer);
+  sectionContainer.classList.add("small-flexbox");
+
+  pair.forEach((person, indexTwo) => {
+    const pairParagraph = document.createElement("p");
+    sectionContainer.appendChild(pairParagraph);
+    pairParagraph.classList.add("members");
+    pairParagraph.innerText = person;
+
+    if (indexTwo == 0) {
+      pairParagraph.classList.add("m1");
+      const plusSvg = document.getElementById("plusSign");
+      sectionContainer.appendChild(plusSvg);
+    } else {
+      pairParagraph.classList.add("m5");
+    }
+  });
+});
--- src/javascript/teams.js
+++ src/javascript/teams.js
@@ -1,10 +1,35 @@
-// const teams = [{'Team 1': [Align, Block, Center, Descendent, End]}, {'Team 2': [Focus, Grid, Hover, Inline, Justify]}]
 
-// teams.forEach((person, index) => { //3.Parameter teams?
-//     let container = document.querySelector('.large-flexbox')
-//     let personBox = document.querySelector('.members')
-//     container.appendChild(personBox)
-//     personBox.classList
+const teams = [
+  ["Team 1", "Align", "Block", "Center", "Descendent", "End"],
+  ["Team 2", "Focus", "Grid", "Hover", "Inline", "Justify"],
+];
 
+const mainContainer = document.querySelector("main");
 
-// })
+teams.forEach((quintet, indexOne) => {
+  const sectionContainer = document.createElement("section");
+  mainContainer.appendChild(sectionContainer);
+  sectionContainer.classList.add("large-flexbox");
+
+  quintet.forEach((item, indexTwo) => {
+    const paragraph = document.createElement("p");
+    sectionContainer.appendChild(paragraph);
+
+    if (indexTwo == 0) {
+        paragraph.classList.add("large-flexbox__header");
+        paragraph.innerText = item
+      } else {
+        paragraph.classList.add("members");
+        paragraph.innerText = item;
+      }
+
+    if (indexTwo == 1) {
+      paragraph.classList.add("m1");
+    } else if (indexTwo >=2 && indexTwo<=4) {
+      paragraph.classList.add("m2");
+    } else if (indexTwo == 5) {
+       paragraph.classList.add('m5')
+    }
+  });
+});
+
