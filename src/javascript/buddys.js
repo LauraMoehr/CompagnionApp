a74ac3fa13e46956865b3c3c970502f94315610b
Laura Moehr <laura.moehr@gmail.com>
2021-11-26 14:15:31 +0100

    Next try at pushing JS

---
+++ src/javascript/buddys.js
@@ -0,0 +1,13 @@
+const buddys = [['Align', 'Block'], [Center, Descendent], [End, Focus], [Grid, Hover], [Inline, Justify]]
+
+// buddys.forEach((person, index) => {
+//     console.log(person);
+//     let container = document.querySelector('.small-flexbox');
+//     let personBox = document.querySelector('.members')
+//     container.appendChild(personBox)
+//     personBox.classList
+            
+            
+// })
+    
+console.log(buddys[0]); 
---
+++ src/javascript/teams.js
@@ -0,0 +1,10 @@
+// const teams = [{'Team 1': [Align, Block, Center, Descendent, End]}, {'Team 2': [Focus, Grid, Hover, Inline, Justify]}]
+
+// teams.forEach((person, index) => { //3.Parameter teams?
+//     let container = document.querySelector('.large-flexbox')
+//     let personBox = document.querySelector('.members')
+//     container.appendChild(personBox)
+//     personBox.classList
+
+
+// })
